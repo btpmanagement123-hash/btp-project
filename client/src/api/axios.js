142af63c45e85b1cf8efcc1df7d1b7d7403ec0d8
student-kajal <kajal971126@gmail.com>
2026-01-25 02:53:08 +0530

    url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,32 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:5000/api',   // direct full URL
+// });
+
+// api.interceptors.request.use(config => {
+//   const token = localStorage.getItem('btp_token');
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
+import axios from "axios";
+
+// Auto switch: localhost pe ho to local backend, warna Render backend
+const API_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000/api"
+    : "https://btp-project-1.onrender.com/api";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',   // direct full URL
+  baseURL: API_URL,
 });
 
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('btp_token');
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("btp_token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
